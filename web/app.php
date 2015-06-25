4d8cbd2978a94ef265073bbf4e9a1e4101fea067
Josh Worden <josh.worden@cygnus.com>
2015-06-25 21:03:47 +0000

    Updated app

--- web/app.php
+++ web/app.php
@@ -2,25 +2,30 @@
 
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
 
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+$env = strtolower(getenv('APP_ENV'));
 
-// Enable APC for autoloading to improve performance.
-// You should change the ApcClassLoader first argument to a unique prefix
-// in order to prevent cache key conflicts with other applications
-// also using APC.
-/*
-$apcLoader = new ApcClassLoader(sha1(__FILE__), $loader);
-$loader->unregister();
-$apcLoader->register(true);
-*/
+$loader = ($env !== 'dev') ? require_once __DIR__.'/../app/bootstrap.php.cache' : require_once __DIR__.'/../app/autoload.php';
+
+// Allow debug mode to be enabled independantly of environment.
+$debug = 'dev' === $env;
+if ($debug) {
+    Debug::enable();
+}
 
 require_once __DIR__.'/../app/AppKernel.php';
-//require_once __DIR__.'/../app/AppCache.php';
 
-$kernel = new AppKernel('prod', false);
-$kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
+// Load environment based on server environment variable 'APP_ENV'
+if ('prod' === $env) {
+    $kernel = new AppKernel('prod', $debug);
+    $kernel->loadClassCache();
+} elseif ('dev' === $env) {
+    $kernel = new AppKernel('dev', $debug);
+} else {
+    $kernel = new AppKernel('test', $debug);
+    $kernel->loadClassCache();
+}
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
